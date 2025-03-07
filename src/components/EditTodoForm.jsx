9b05fac293dba5e81429a039748e29825c2be37c
Wei(William) Wang <william.wang.uncharted@gmail.com>
2025-03-07 14:41:00 -0600

    Update the edit input section

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const EditTodoForm = ({ editTodo, detail }) => {
   const [value, setvalue] = useState(detail.task);
+  const editInputRef = useRef();
+
+  useEffect(() => {
+    editInputRef.current.focus();
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     editTodo(value, detail.id);
     setvalue('');
   };
 
+
+
   return (
-    <form className="TodoForm" onSubmit={handleSubmit}>
+    <form className="EditTodoForm" onSubmit={handleSubmit}>
       <input
         type="text"
-        className="todo-input"
+        className="edit-todo-input"
+        ref={editInputRef}
         value={value}
-        placeholder="What is the task today?"
+        placeholder="What do you want to update?"
         onChange={e => setvalue(e.target.value)}
       />
-      <button type="submit" className="todo-btn">Update Task</button>
+      <button type="submit" className="todo-btn edit-todo-btn" >Update Task</button>
     </form>
   );
 };
